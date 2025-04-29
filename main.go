1bf277b95898dda8b8921e1685d3bfca2474f4a4
Md. Tanvirul Islam <md.tanvirulislam@outlook.com>
2025-04-29 08:07:38 +0600

    infinite loop

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
+	// var bookings [50]string
+	// var bookingsSlice []string
+	// var bookingsSlice = []string{}
+	bookingsSlice := []string{}
 
 	fmt.Printf("conferenceName is %T, conferenceTickets is %T, remainingTickets is %T\n", conferenceName, conferenceTickets, remainingTickets)
 
@@ -17,45 +21,53 @@ func main() {
 	// fmt.Println(conferenceName);
 	// fmt.Println(&conferenceName);
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
-	var bookings [50]string
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+		var fullName string
 
-	// ask user for their name
-	fmt.Println("Enter your First Name:")
-	fmt.Scan(&firstName)
+		// ask user for their name
+		fmt.Println("Enter your First Name:")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your Last Name:")
-	fmt.Scan(&lastName)
-	
-	fmt.Println("Enter your Email:")
-	fmt.Scan(&email)
-	
-	fmt.Println("Enter number of tickets, you want:")
-	fmt.Scan(&userTickets)
-	
-	remainingTickets = remainingTickets - userTickets;
+		fmt.Println("Enter your Last Name:")
+		fmt.Scan(&lastName)
 
-	bookings[0] = firstName + " " + lastName
-	
-	fmt.Println("")
-	fmt.Println("**********************")
-	fmt.Println("")
+		fmt.Println("Enter your Email:")
+		fmt.Scan(&email)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will get a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+		fmt.Println("Enter number of tickets, you want:")
+		fmt.Scan(&userTickets)
 
-	fmt.Printf("%v tickets remain out of %v\n", remainingTickets, conferenceTickets);
+		remainingTickets = remainingTickets - userTickets
 
-	fmt.Printf("Booking:\n %v \n", bookings);
-	fmt.Printf("No of person make booking: %v \n", len(bookings));
+		fullName = firstName + " " + lastName
+		// bookings[0] = fullName
+		// bookingsSlice = append(bookingsSlice, fullName)
+		bookingsSlice = append(bookingsSlice, fullName)
 
-	fmt.Println(bookings);
-	// fmt.Println(bookings[2]);
-	// fmt.Println(bookings[3]);
+		fmt.Println("")
+		fmt.Println("**********************")
+		fmt.Println("")
 
-	fmt.Println("")
-	fmt.Println("**********************")
-	fmt.Println("")
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will get a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+
+		fmt.Println("")
+
+		fmt.Printf("%v tickets remain out of %v\n", remainingTickets, conferenceTickets)
+
+		fmt.Println("")
+
+		fmt.Printf("No of person make booking: %v \n", len(bookingsSlice))
+
+		fmt.Println("")
+
+		fmt.Printf("Booking:\n %v \n", bookingsSlice)
+
+		fmt.Println("")
+		fmt.Println("**********************")
+		fmt.Println("")
+	}
 }
